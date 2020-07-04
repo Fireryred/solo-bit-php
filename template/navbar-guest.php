9e0f1c0febfadcd760234ce55bc4a819c858401a
Gershom Gruta <gershomgruta06@gmail.com>
2020-07-04 08:49:52 +0800

    added template

---
+++ template/navbar-guest.php
@@ -0,0 +1,14 @@
+<div class="navbar navbar-expand navbar-dark bg-dark sticky-top">
+	<div class="navbar-brand" href="index.php">Solo Bit</div>
+	<div class="collapse navbar-collapse">
+		<ul class="navbar-nav mr-auto">
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" name="product" placeholder="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+		<a href="login.php" class="nav-link text-light">Login</a>
+		<span class="navbar-text">or</span>
+		<a href="register.php" class="nav-link text-light">Register</a>
+	</div>
+</div>
---
+++ template/navbar-user.php
@@ -0,0 +1,19 @@
+<div class="navbar navbar-expand navbar-dark bg-dark sticky-top">
+	<div class="navbar-brand" href="index.php">Solo Bit</div>
+	<div class="collapse navbar-collapse">
+		<ul class="navbar-nav mr-auto">
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" name="product" placeholder="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+		<span class="dropdown">
+			<a href="" class="nav-link text-light dropdown-toggle" data-toggle="dropdown">User</a>
+			<div class="dropdown-menu">
+				<a href="" class="dropdown-item">Account</a>
+				<a href="" class="dropdown-item">Something</a>
+				<a href="" class="dropdown-item">Logout</a>
+			</div>
+		</span>
+	</div>
+</div>
