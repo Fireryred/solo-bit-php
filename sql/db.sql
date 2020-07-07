3ece80a74566a30f1a931e3802b59d1ffb230653
Gershom Gruta <gershomgruta06@gmail.com>
2020-07-07 19:57:49 +0800

    create table for user_details

---
+++ sql/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS user_details(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(255),
+	password VARCHAR(255),
+	email VARCHAR(255),
+	full_name VARCHAR(255)
+);
