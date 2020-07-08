8f9c3e64ef8521f9ea9a4ebd60a1e0c369e64014
Gershom Gruta <gershomgruta06@gmail.com>
2020-07-08 11:24:50 +0800

    added search-process

--- template/navbar-guest.php
+++ template/navbar-guest.php
@@ -3,7 +3,7 @@
 	<div class="collapse navbar-collapse">
 		<ul class="navbar-nav mr-auto">
 		</ul>
-		<form class="form-inline my-2 my-lg-0">
+		<form class="form-inline my-2 my-lg-0" action="search-process.php">
 			<input class="form-control mr-sm-2" type="search" name="product" placeholder="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		</form>
