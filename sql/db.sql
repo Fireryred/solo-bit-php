0e7229b9bfe0df723c60bab7b9921d619e677251
Gershom Gruta <gershomgruta06@gmail.com>
2020-07-08 11:23:07 +0800

    fix title

--- sql/db.sql
+++ sql/db.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS user_details(
 	email VARCHAR(255),
 	full_name VARCHAR(255)
 );
+CREATE TABLE IF NOT EXISTS product(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255),
+	image VARCHAR(255),
+	price DECIMAL(10,0)
+);
+CREATE TABLE IF NOT EXISTS product_category(
+	category VARCHAR(255),
+	product_id INT(6) UNSIGNED,
+	FOREIGN KEY (product_id) REFERENCES product(id)
+);
