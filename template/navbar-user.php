b473868aae6747b690afef17e96d695812c17c45
Gershom Gruta <gershomgruta06@gmail.com>
2020-07-08 11:25:35 +0800

    fix dropbox, add search-process

--- template/navbar-user.php
+++ template/navbar-user.php
@@ -3,15 +3,15 @@
 	<div class="collapse navbar-collapse">
 		<ul class="navbar-nav mr-auto">
 		</ul>
-		<form class="form-inline my-2 my-lg-0">
+		<form class="form-inline my-2 my-lg-0" action="search-process.php">
 			<input class="form-control mr-sm-2" type="search" name="product" placeholder="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		</form>
-		<span class="dropdown col-1">
+		<span class="dropdown">
 			<a href="" class="nav-link text-light dropdown-toggle" data-toggle="dropdown"><?=$_SESSION['username']?></a>
-			<div class="dropdown-menu">
+			<div class="dropdown-menu dropdown-menu-right">
 				<a href="" class="dropdown-item">Account</a>
-				<a href="" class="dropdown-item">Something</a>
+				<a href="" class="dropdown-item">Checkout</a>
 				<a href="logout.php" class="dropdown-item">Logout</a>
 			</div>
 		</span>
