db203159072559841da24e7e86ade11043a21876
Gershom Gruta <gershomgruta06@gmail.com>
2020-07-11 09:44:55 +0800

    fix template

--- template/navbar-guest.php
+++ template/navbar-guest.php
@@ -3,8 +3,8 @@
 	<div class="collapse navbar-collapse">
 		<ul class="navbar-nav mr-auto">
 		</ul>
-		<form class="form-inline my-2 my-lg-0" action="search-process.php">
-			<input class="form-control mr-sm-2" type="search" name="product" placeholder="Search">
+		<form class="form-inline my-2 my-lg-0" action="search.php">
+			<input class="form-control mr-sm-2" type="search" name="s" placeholder="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		</form>
 		<a href="login.php" class="nav-link text-light">Login</a>
--- template/navbar-user.php
+++ template/navbar-user.php
@@ -3,15 +3,15 @@
 	<div class="collapse navbar-collapse">
 		<ul class="navbar-nav mr-auto">
 		</ul>
-		<form class="form-inline my-2 my-lg-0" action="search-process.php">
-			<input class="form-control mr-sm-2" type="search" name="product" placeholder="Search">
+		<form class="form-inline my-2 my-lg-0" action="search.php">
+			<input class="form-control mr-sm-2" type="search" name="s" placeholder="Search">
 			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		</form>
 		<span class="dropdown">
 			<a href="" class="nav-link text-light dropdown-toggle" data-toggle="dropdown"><?=$_SESSION['username']?></a>
 			<div class="dropdown-menu dropdown-menu-right">
 				<a href="" class="dropdown-item">Account</a>
-				<a href="" class="dropdown-item">Checkout</a>
+				<a href="view-cart.php" class="dropdown-item">View Cart</a>
 				<a href="logout.php" class="dropdown-item">Logout</a>
 			</div>
 		</span>
